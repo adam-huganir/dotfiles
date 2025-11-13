26fdd462bc458fbccfa17576d57643bbb06c07ad
Adam Huganir <adam@huganir.com>
2025-11-13 16:10:15 -0500

    wip back to starship

--- profile.ps1
+++ profile.ps1
@@ -1,4 +1,5 @@
-Invoke-Expression (&starship init powershell)
+# Set Python IO encoding to utf-8
+$env:PYTHONIOENCODING = "utf-8"
 
 # Shows navigable menu of all options when hitting Tab
 Set-PSReadlineKeyHandler -Key Tab -Function MenuComplete
@@ -7,7 +8,40 @@ Set-PSReadlineKeyHandler -Key Tab -Function MenuComplete
 Set-PSReadlineKeyHandler -Key UpArrow -Function HistorySearchBackward
 Set-PSReadlineKeyHandler -Key DownArrow -Function HistorySearchForward
 
-# Set Python IO encoding to utf-8
-$env:PYTHONIOENCODING="utf-8"
+# Custom Functions
+function Trace-Read
+{
+    param(
+        [Parameter(Mandatory = $true, Position = 0)]
+        [string]$DocId,
 
-iex "$(thefuck --alias oops)"
+        [Parameter(Position = 1)]
+        [string]$Environment = "production"
+    )
+
+    $docIdLower = $DocId.ToLower()
+
+    gcloud --project "redshred-$Environment" logging read --format "value(textPayload)" @"
+     resource.labels.cluster_name=~"$Environment"
+     resource.labels.container_name="read"
+     resource.labels.pod_name=~"$docIdLower"
+"@
+}
+
+function Which
+{
+    param(
+        [Parameter(Mandatory = $true, Position = 0)]
+        [string]$Command
+    )
+
+    $CommandObject = Get-Command $Command -ErrorAction SilentlyContinue
+    if ($null -ne $CommandObject)
+    {
+        $CommandObject.Path
+    }
+}
+
+#Invoke-Expression "$( thefuck --alias oops )"
+$env:STARSHIP_CONFIG = "$HOME\dotfiles\starship.toml"
+Invoke-Expression (&starship init powershell)
